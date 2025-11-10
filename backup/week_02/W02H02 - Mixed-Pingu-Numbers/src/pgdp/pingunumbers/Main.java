950dc857eb3e2435e3fccafade941d3a2e7e9b96
Zhiyuan-Jin <philippking2006@gmail.com>
2025-11-10 05:15:34 +0100

    Backup all assignments 2025-11-10 05:15:34

---
+++ backup/week_02/W02H02 - Mixed-Pingu-Numbers/src/pgdp/pingunumbers/Main.java
@@ -0,0 +1,16 @@
+package pgdp.pingunumbers;
+
+public class Main {
+
+    //TODO 5.0: Uncomment and extend to test your implementation using the main method below.
+    public static void main(String[] args) {
+       /* MixedPinguNumber p1 = new MixedPinguNumber(7, 2, 3); // 3 + 7/2 = 6 1/2 expected
+        p1.prettyPrint();
+        p1.extractWholePart();
+        p1.prettyPrint(); // Expected: 6 1/2
+        System.out.println();
+
+        */
+
+    }
+}
---
+++ backup/week_02/pgdp2526w02h03-go97hoc/src/pgdp/saleuine/Kaufuin.java
@@ -0,0 +1,44 @@
+package pgdp.saleuine;
+
+public class Kaufuin {
+    private String name;
+    private int age;
+    private double money;
+    private Order order;
+
+    public Kaufuin(String name, int age, double money, Order order) {
+        this.name = name;
+        this.age = age;
+        this.money = money;
+        this.order = order;
+    }
+
+    public Order getOrder() {
+        // TODO - Task "Kaufuin Order-Methoden"
+        return order;
+    }
+
+    public void giveNewOrder(Order newOrder) {
+        // TODO - Task "Kaufuin Order-Methoden"
+       this.order=newOrder;
+    }
+
+    public void addToOrder(Order additionalOrder) {
+        // TODO - Task "Kaufuin Order-Methoden"
+        order.addOrder(additionalOrder);
+    }
+
+    public String giveInformation() {
+        // TODO - Task "Kaufuin giveInformation"
+        return name + "(" + age + ") hätte gerne " +
+                order.getAmountCrustaceans() + " Krustentiere, " +
+                order.getAmountAnchovies() + " Sardellen und " +
+                order.getAmountSardines() + " Sardinen.";
+    }
+
+    public void pay(double amount) {
+        money-=amount;
+        System.out.println(name + " zahlt " + amount + " und hat noch " + money + "PD übrig.");
+        // TODO - Task "Kaufuin pay"
+    }
+}
