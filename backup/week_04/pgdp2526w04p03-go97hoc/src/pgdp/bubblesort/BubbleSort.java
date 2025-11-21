18ee106f66881cb5be094053aea764b9f99c2e16
Zhiyuan-Jin <philippking2006@gmail.com>
2025-11-21 14:42:04 +0100

    Backup all assignments 2025-11-21 14:42:04

---
+++ backup/week_04/pgdp2526w04p03-go97hoc/src/pgdp/bubblesort/BubbleSort.java
@@ -0,0 +1,18 @@
+package pgdp.bubblesort;
+
+public class BubbleSort {
+
+	//TODO 1.0: Implement the method to sort a given array in ascending order.
+	public static void bubbleSort(int[] fish) {
+        for (int i = 0; i < fish.length ; i++) {
+            for (int j = 0; j < fish.length-1; j++) {
+                if(fish[j]>fish[j+1]){
+                    int a=fish[j];
+                    fish[j]=fish[j+1];
+                    fish[j + 1] = a;
+                }
+
+            }
+        }
+	}
+}
